d7a4e59ce53fae8ad7f3cca6a859ec4465acbdac
Ben Pyungwoo Yoo <hovan@hovans.com>
2024-07-03 00:36:48 +0900

    Add video files

--- pp3d/src/components/RenderingSamples/index.tsx
+++ pp3d/src/components/RenderingSamples/index.tsx
@@ -5,25 +5,25 @@ import { OrbitControls, useGLTF, useAnimations } from "@react-three/drei";
 import styles from "./styles.module.css";
 
 type RenderingItem = {
-  youtubeURL: string;
-  glbURL: string;
+  video: string;
+  glb: string;
   title: string;
 };
 
 const RenderingList: RenderingItem[] = [
   {
-    youtubeURL: "https://www.youtube.com/embed/rZPtUUlVLRk",
-    glbURL: "/glb/1.glb",
+    video: "/video/1.mp4",
+    glb: "/glb/1.glb",
     title: "테니스",
   },
   {
-    youtubeURL: "https://youtube.com/embed/ysE834gGPJk",
-    glbURL: "/glb/2.glb",
+    video: "/video/2.mp4",
+    glb: "/glb/2.glb",
     title: "골프",
   },
   {
-    youtubeURL: "https://youtube.com/embed/tZAbY33UOvE",
-    glbURL: "/glb/3.glb",
+    video: "/video/3.mp4",
+    glb: "/glb/3.glb",
     title: "웨이트 트레이닝",
   },
 ];
@@ -42,7 +42,7 @@ function GLBModel({ url }: { url: string }) {
   return <primitive ref={group} object={scene} />;
 }
 
-function RenderingSample({ youtubeURL, glbURL, title }: RenderingItem) {
+function RenderingSample({ video, glb, title }: RenderingItem) {
   return (
     <div className="column">
       <div className={clsx("row", styles.title)}>
@@ -51,20 +51,16 @@ function RenderingSample({ youtubeURL, glbURL, title }: RenderingItem) {
       <div className="row">
         <div className="col col--2" />
         <div className={clsx("col col--4", styles.videoContainer)}>
-          <iframe
-            width="350"
-            height="530"
-            src={youtubeURL}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+        <video width="350" height="530" controls>
+            <source src={video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
         <div className={clsx("col col--4", styles.threeJsRender)}>
           <Canvas camera={{ position: [0, 0, 5] }}>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <GLBModel url={glbURL} />
+            <GLBModel url={glb} />
             <OrbitControls />
           </Canvas>
         </div>
